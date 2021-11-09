f3c2da118378d1fc4c44a6ee0be03d81e5bdbcab
Julia-Korol <korol.julia@inbox.ru>
2021-11-09 16:12:20 +0300

    Finished project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ol class="questions">
+      <li v-for="(question, index) in questions" :key="question.question">
+        <Question
+          :question="question"
+          :isAnswered="!!answeredQuestions[index]"
+          :questionIndex="index"
+          @change="selectAnswer(question, index, ...arguments)"
+        />
+      </li>
+    </ol>
+
+    <p class="result" v-if="isResultShown">
+      <span class="result__title">Result: </span>
+      {{ correctAnswers }}/{{ questions.length }}
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Question from "@/components/Question.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Question },
+  data() {
+    return {
+      questions: [],
+      answeredQuestions: {},
+    };
+  },
+  computed: {
+    isResultShown() {
+      const questionsLength = this.questions.length;
+
+      return (
+        questionsLength &&
+        Object.keys(this.answeredQuestions).length === questionsLength
+      );
+    },
+    correctAnswers() {
+      return Object.values(this.answeredQuestions).filter(
+        ({ isCorrect }) => isCorrect
+      ).length;
+    },
+  },
+  methods: {
+    async getQuestions() {
+      const data = await fetch(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
+      const response = await data.json();
+
+      this.questions = response.results.map(
+        ({ question, correct_answer, incorrect_answers }) => ({
+          question,
+          correctAnswer: correct_answer,
+          answers: [...incorrect_answers, correct_answer].sort(
+            () => 0.5 - Math.random()
+          ),
+        })
+      );
+    },
+    async selectAnswer({ question, correctAnswer }, index, answer) {
+      await fetch("http://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          question: question,
+          answer: answer,
+        }),
+      });
+
+      this.answeredQuestions = {
+        ...this.answeredQuestions,
+        [index]: { isCorrect: correctAnswer === answer },
+      };
+    },
+  },
+  mounted() {
+    this.getQuestions();
   },
 };
 </script>
@@ -21,8 +88,21 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.questions {
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+.result {
+  text-align: center;
+  font-size: 20px;
+  color: darkorange;
+}
+
+.result__title {
+  font-weight: bold;
 }
 </style>
---
+++ src/components/Question.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="question">
+    <p class="question__title" v-html="question.question"></p>
+    <div class="question__answers">
+      <label
+        class="question__answer"
+        v-for="answer in question.answers"
+        :key="answer"
+      >
+        <input
+          type="radio"
+          @change="$emit('change', answer)"
+          :value="answer"
+          :name="'Question-' + questionIndex + 1"
+          :disabled="isAnswered"
+        />
+        <span v-html="answer" />
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+    questionIndex: {
+      type: Number,
+      required: true,
+    },
+    isAnswered: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.question {
+  margin-bottom: 30px;
+}
+
+.question__title {
+  font-weight: bold;
+  margin-bottom: 20px;
+  line-height: 26px;
+}
+
+.question__answers {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+}
+</style>
